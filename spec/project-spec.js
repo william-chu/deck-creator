3fbe060f94a47afed0f8d4f98a6638be857c7745
William Chu <Williamchu51@gmail.com>
2018-07-25 16:08:47 -0700

    add test for shuffling deck

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -7,8 +7,15 @@ describe('Deck', function() {
     reusableDeck = new Deck();
   });
 
-  it('Deck of cards contains 52 array items', function() {
-    console.log(reusableDeck);
+  it('should generate a deck of cards that contains 52 array items', function() {
+    // console.log(reusableDeck);
     expect(reusableDeck.cards.length).toEqual(52);
   });
+
+  it('should shuffle a deck that does not equal new deck', function() {
+    let shuffledDeck = reusableDeck.shuffle();
+    console.log(shuffledDeck.cards);
+    expect(shuffledDeck.cards).not.toEqual(reusableDeck.cards);
+  });
+
 });
