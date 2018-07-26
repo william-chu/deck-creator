e9d32aec8954d20ae1c5be3b941c796c69b3837f
William Chu <Williamchu51@gmail.com>
2018-07-25 19:05:36 -0700

    begin creating functions to display cards

--- src/main.js
+++ src/main.js
@@ -1,2 +1,34 @@
 import { Deck } from './Deck';
 import './styles.css';
+
+function shufflePress() {
+  displayDeck.shuffle();
+}
+
+function sortPress() {
+  displayDeck.sortByValue();
+}
+
+function displayDeck() {
+  let cardGrid = document.getElementById('card-grid');
+  cardsArr.forEach((card) => {
+    let cardToDisplay = document.createElement("div");
+	  cardToDisplay.className = "card";
+	  card.appendChild(card[0]);
+	  card.appendChild(card[1]);
+    cardGrid.appendChild(cardToDisplay);
+  })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let displayDeck = new Deck();
+  let cardsArr = displayDeck.cards;
+  let cardGrid = document.getElementById('card-grid');
+  cardsArr.forEach((card) => {
+    let cardToDisplay = document.createElement("div");
+	  cardToDisplay.className = "card";
+	  cardToDisplay.append(card[0]);
+	  cardToDisplay.append(card[1]);
+    cardGrid.appendChild(cardToDisplay);
+  })
+});
