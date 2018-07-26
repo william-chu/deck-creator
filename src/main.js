c1cea527d883fb1a46330d3002ef2d8efdd3d2c8
William Chu <Williamchu51@gmail.com>
2018-07-25 19:47:45 -0700

    add frontend to allow user to shuffle and sort cards

--- src/main.js
+++ src/main.js
@@ -1,34 +1,36 @@
 import { Deck } from './Deck';
 import './styles.css';
 
-function shufflePress() {
-  displayDeck.shuffle();
-}
-
-function sortPress() {
-  displayDeck.sortByValue();
-}
-
-function displayDeck() {
-  let cardGrid = document.getElementById('card-grid');
-  cardsArr.forEach((card) => {
-    let cardToDisplay = document.createElement("div");
-	  cardToDisplay.className = "card";
-	  card.appendChild(card[0]);
-	  card.appendChild(card[1]);
-    cardGrid.appendChild(cardToDisplay);
-  })
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-  let displayDeck = new Deck();
-  let cardsArr = displayDeck.cards;
-  let cardGrid = document.getElementById('card-grid');
-  cardsArr.forEach((card) => {
-    let cardToDisplay = document.createElement("div");
-	  cardToDisplay.className = "card";
-	  cardToDisplay.append(card[0]);
-	  cardToDisplay.append(card[1]);
-    cardGrid.appendChild(cardToDisplay);
-  })
+  let deck = new Deck();
+  const displayDeck = (cardsArr) => {
+    let cardGrid = document.getElementById('card-grid');
+    cardGrid.innerHTML = "";
+    cardsArr.forEach((card) => {
+      let cardToDisplay = document.createElement("div");
+      let symbol;
+      if (card[1] === 'hearts') {
+        symbol = '♥';
+      } else if (card[1] === 'diamonds') {
+        symbol = '♦';
+      } else if (card[1] === 'clubs') {
+        symbol = '♣';
+      } else {
+        symbol = '♠';
+      }
+  	  cardToDisplay.className = "card";
+  	  cardToDisplay.append(card[0]);
+  	  cardToDisplay.append(symbol);
+      cardGrid.appendChild(cardToDisplay);
+    })
+  }
+  document.getElementById('shuffle').addEventListener('click', function() {
+    deck.shuffle();
+    displayDeck(deck.cards);
+  });
+  document.getElementById('sort').addEventListener('click', function() {
+    deck.sortByValue();
+    displayDeck(deck.cards);
+  });
+  displayDeck(deck.cards);
 });
